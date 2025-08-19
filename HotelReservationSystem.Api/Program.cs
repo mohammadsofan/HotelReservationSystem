22e3a147af4e97b016a1c0f15b75663db7a80edb
mohammadsofan <nassersofan@gmail.com>
2025-08-19 19:23:31 +0300

    feat: configure swagger to use jwt token for authorization

--- HotelReservationSystem.Api/Program.cs
+++ HotelReservationSystem.Api/Program.cs
@@ -3,6 +3,7 @@ using HotelReservationSystem.Application.Extensions;
 using HotelReservationSystem.Application.Settings;
 using HotelReservationSystem.Infrastructure.Extensions;
 using HotelReservationSystem.Infrastructure.Seeders;
+using Microsoft.OpenApi.Models;
 namespace HotelReservationSystem.Api
 {
     public class Program
@@ -16,7 +17,36 @@ namespace HotelReservationSystem.Api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' followed by your JWT token.\nExample: Bearer abcdef12345"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Authorization",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                    });
+            });
             builder.Services.AddApplication();
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.Configure<JwtSettings>(
