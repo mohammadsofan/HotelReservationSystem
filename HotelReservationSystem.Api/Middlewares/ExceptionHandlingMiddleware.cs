1a0bb9d740297766ad44439f6a9847f95dc8dd6e
mohammadsofan <nassersofan@gmail.com>
2025-08-22 16:42:27 +0300

    feat(logging): add logging to ExceptionHandlingMiddleware

--- HotelReservationSystem.Api/Middlewares/ExceptionHandlingMiddleware.cs
+++ HotelReservationSystem.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -7,6 +7,11 @@ namespace HotelReservationSystem.Api.Middlewares
 {
     public class ExceptionHandlingMiddleware : IMiddleware
     {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
@@ -15,6 +20,7 @@ namespace HotelReservationSystem.Api.Middlewares
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Exception caught in middleware: {ExceptionType} - {Message}", ex.GetType().Name, ex.Message);
                 await HandleExceptionsAsync(context, ex);
             }
         }
@@ -31,7 +37,7 @@ namespace HotelReservationSystem.Api.Middlewares
                         response = ApiResponse.Fail(nfException.Message);
                         break;
                     }
-                case ValidationException validationException: 
+                case ValidationException validationException:
                     {
                         statusCode = HttpStatusCode.BadRequest;
                         response = ApiResponse.Fail(validationException.Message,
@@ -59,11 +65,13 @@ namespace HotelReservationSystem.Api.Middlewares
                     }
                 default:
                     {
+                        _logger.LogError(ex, "Handled exception: {ExceptionType} - {Message}", ex.GetType().Name, ex.Message);
                         statusCode = HttpStatusCode.InternalServerError;
                         response = ApiResponse.Fail($"Unexpected error occured. {ex.Message} {ex.InnerException?.Message}");
                         break;
                     }
             }
+            _logger.LogWarning(ex, "Handled exception: {ExceptionType} - {Message}", ex.GetType().Name, ex.Message);
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)statusCode;
             var json = JsonSerializer.Serialize(response);
