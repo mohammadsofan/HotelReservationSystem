5e2fc39ec0a58db6660086378c850d2b580557b6
mohammadsofan <nassersofan@gmail.com>
2025-08-22 15:18:50 +0300

    feat(logging): add logging to all facility handlers

--- HotelReservationSystem.Application/Handlers/Facility/CreateFacilityHandler.cs
+++ HotelReservationSystem.Application/Handlers/Facility/CreateFacilityHandler.cs
@@ -3,22 +3,28 @@ using HotelReservationSystem.Application.Dtos.Facility.Responses;
 using HotelReservationSystem.Application.Interfaces;
 using Mapster;
 using MediatR;
+using Microsoft.Extensions.Logging;
+
 namespace HotelReservationSystem.Application.Handlers.Facility
 {
     internal class CreateFacilityHandler : IRequestHandler<CreateFacilityCommand, FacilityResponseDto>
     {
         private readonly IFacilityRepository _facilityRepository;
+        private readonly ILogger<CreateFacilityHandler> _logger;
 
-        public CreateFacilityHandler(IFacilityRepository facilityRepository)
+        public CreateFacilityHandler(IFacilityRepository facilityRepository, ILogger<CreateFacilityHandler> logger)
         {
             _facilityRepository = facilityRepository;
+            _logger = logger;
         }
 
         public async Task<FacilityResponseDto> Handle(CreateFacilityCommand request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Starting facility creation.");
             var requestDto = request.RequestDto;
             var facility = requestDto.Adapt<Domain.Entities.Facility>();
             facility = await _facilityRepository.CreateAsync(facility, cancellationToken);
+            _logger.LogInformation("Facility created successfully with ID {FacilityId}.", facility.Id);
             return facility.Adapt<FacilityResponseDto>();
         }
     }
--- HotelReservationSystem.Application/Handlers/Facility/GetFacilitiesByFilterHandler.cs
+++ HotelReservationSystem.Application/Handlers/Facility/GetFacilitiesByFilterHandler.cs
@@ -3,21 +3,27 @@ using HotelReservationSystem.Application.Interfaces;
 using HotelReservationSystem.Application.Queries.Facility;
 using Mapster;
 using MediatR;
+using Microsoft.Extensions.Logging;
+
 namespace HotelReservationSystem.Application.Handlers.Facility
 {
     public class GetFacilitiesByFilterHandler : IRequestHandler<GetFacilitiesByFilterQuery, ICollection<FacilityResponseDto>>
     {
         private readonly IFacilityRepository _facilityRepository;
+        private readonly ILogger<GetFacilitiesByFilterHandler> _logger;
 
-        public GetFacilitiesByFilterHandler(IFacilityRepository facilityRepository)
+        public GetFacilitiesByFilterHandler(IFacilityRepository facilityRepository, ILogger<GetFacilitiesByFilterHandler> logger)
         {
             _facilityRepository = facilityRepository;
+            _logger = logger;
         }
 
         public async Task<ICollection<FacilityResponseDto>> Handle(GetFacilitiesByFilterQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Retrieving facilities with provided filter.");
             var filter = request.Filter;
             var facilities = await _facilityRepository.GetAllByFilterAsync(filter);
+            _logger.LogInformation("Retrieved {Count} facilities.", facilities.Count());
             return facilities.Select(r => r.Adapt<FacilityResponseDto>()).ToList();
         }
     }
