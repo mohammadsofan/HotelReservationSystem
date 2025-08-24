54ad410dfa66a3879773c4109d9f92220d1fafd4
mohammadsofan <nassersofan@gmail.com>
2025-08-24 13:28:35 +0300

    feat(caching): add caching to account controller

--- HotelReservationSystem.Api/Controllers/AccountController.cs
+++ HotelReservationSystem.Api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using HotelReservationSystem.Application.Commands.User;
 using HotelReservationSystem.Application.Dtos.User.Requests;
 using HotelReservationSystem.Application.Dtos.User.Responses;
 using HotelReservationSystem.Application.Queries.User;
+using HotelReservationSystem.Application.Interfaces;
 using HotelReservationSystem.Domain.Constants;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -16,27 +17,46 @@ namespace HotelReservationSystem.Api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ICacheService _cacheService;
         private readonly ILogger<AccountController> _logger;
-
-        public AccountController(IMediator mediator, ILogger<AccountController> logger)
+        private readonly string AllUsersCacheKey = "AllUsers";
+        public AccountController(IMediator mediator, ICacheService cacheService, ILogger<AccountController> logger)
         {
             _mediator = mediator;
+            _cacheService = cacheService;
             _logger = logger;
         }
         [HttpGet]
         public async Task<IActionResult> GetAllUsersAsync()
         {
             _logger.LogInformation("Getting all users.");
+            var userDtos = await _cacheService.GetAsync<IEnumerable<UserResponseDto>>(AllUsersCacheKey);
+            if (userDtos != null)
+            {
+                _logger.LogInformation("Users found in cache.");
+                return Ok(ApiResponse<IEnumerable<UserResponseDto>>.Ok("Users retrieved successfully.", userDtos));
+            }
             var res = await _mediator.Send(new GetUsersByFilterQuery());
             _logger.LogInformation("Users retrieved successfully.");
+            await _cacheService.SetAsync<IEnumerable<UserResponseDto>>(AllUsersCacheKey, res);
+            _logger.LogInformation("Users cached successfully.");
             return Ok(ApiResponse<IEnumerable<UserResponseDto>>.Ok("Users retrieved successfully.", res));
         }
         [HttpGet("{id}",Name = "GetUserByIdAsync")]
         public async Task<IActionResult> GetUserByIdAsync([FromRoute] long id)
         {
             _logger.LogInformation("Getting user by ID {UserId}.", id);
+            var cacheKey = $"User_{id}";
+            var userDto = await _cacheService.GetAsync<UserResponseDto>(cacheKey);
+            if (userDto != null)
+            {
+                _logger.LogInformation("User found in cache.");
+                return Ok(ApiResponse<UserResponseDto>.Ok("User retrieved successfully.", userDto));
+            }
             var res = await _mediator.Send(new GetOneUserByFilterQuery(u => u.Id == id));
             _logger.LogInformation("User retrieved successfully.");
+            await _cacheService.SetAsync<UserResponseDto>(cacheKey, res);
+            _logger.LogInformation("User cached successfully.");
             return Ok(ApiResponse<UserResponseDto>.Ok("User retrieved successfully.", res));
         }
         [HttpDelete("{id}")]
@@ -45,6 +65,10 @@ namespace HotelReservationSystem.Api.Controllers
             _logger.LogInformation("Deleting user with ID {UserId}.", id);
             var res = await _mediator.Send(new DeleteUserCommand(id));
             _logger.LogInformation("User deleted successfully.");
+            var cacheKey = $"User_{id}";
+            await _cacheService.RemoveAsync(cacheKey);
+            await _cacheService.RemoveAsync(AllUsersCacheKey);
+            _logger.LogInformation("User cache removed successfully.");
             return NoContent();
         }
         [HttpPatch("{id}")]
@@ -53,6 +77,10 @@ namespace HotelReservationSystem.Api.Controllers
             _logger.LogInformation("Updating user with ID {UserId}.", id);
             await _mediator.Send(new UpdateUserCommand(id, user));
             _logger.LogInformation("User updated successfully.");
+            var cacheKey = $"User_{id}";
+            await _cacheService.RemoveAsync(cacheKey);
+            await _cacheService.RemoveAsync(AllUsersCacheKey);
+            _logger.LogInformation("User cache removed successfully.");
             return NoContent();
         }
         [HttpPost("login")]
@@ -71,6 +99,10 @@ namespace HotelReservationSystem.Api.Controllers
             _logger.LogInformation("Registering user with email {Email}.", request.Email);
             var res = await _mediator.Send(new CreateUserCommand(request));
             _logger.LogInformation("User registered successfully.");
+            var cacheKey = $"User_{res.Id}";
+            await _cacheService.RemoveAsync(AllUsersCacheKey);
+            await _cacheService.RemoveAsync(cacheKey);
+            _logger.LogInformation("User cache removed successfully.");
             return CreatedAtRoute(nameof(GetUserByIdAsync), new { id = res.Id }, ApiResponse<UserResponseDto>.Ok("User registered successfully.", res));
         }
 
